bc01ec5b2f4c64401dcb68a677a20096729515b4
JessyJamesPasay20 <93660089+JessyJamesPasay20@users.noreply.github.com>
2022-05-29 11:23:20 +0200

    PokemonFighter
    Bug fixes, animation / styling changes, fine tuning code

--- js/script.js
+++ js/script.js
@@ -1,223 +1,387 @@
-var startGame = document.querySelector("#start");
-var introGame = document.querySelector("#intro");
-var pokemonPick = document.querySelector("#selectPokemon");
-var pikachuB = document.querySelector("#pikachuButton");
-var charmanderB = document.querySelector("#charmanderButton");
-var gengarB = document.querySelector("#gengarButton");
-var heroI = document.querySelector("#heroImage");
-var heroN = document.querySelector("#heroName");
-var enemyN = document.querySelector("#enemyName");
-var enemyI = document.querySelector("#enemyImage");
-var heroHp =  document.getElementById('heroHpBar');
-var enemyHp =  document.getElementById('enemyHpBar');
+/* A pokemon 1v1 fighter, first pokemon that inflicts a killing blow wins the game. ( Killing blow == 0hp) */
 
-class pokemon {
+var startGame = document.querySelector("#start"),
+    introGame = document.querySelector("#intro"),
+    selectPokemon = document.querySelector("#selectPokemon"),
+    corviknightButton = document.querySelector("#corviknightButton"),
+    heroImg = document.querySelector("#heroImage"),
+    enemyImg = document.querySelector("#enemyImage"),
+    heroHp =  document.getElementById('heroHpBar'),
+    enemyHp =  document.getElementById('enemyHpBar'),
+    enemyInfo = document.getElementById("enemyInfo"),
+    heroInfo = document.getElementById("heroInfo"),
+    battleText = document.querySelector("#battleText"),
+    heroAttacks = document.querySelector("#heroAttacks"),
+    backgroundImage = document.querySelector("main"),
+    backgroundImages = ['./assets/background-1.png', './assets/background-2.png'];
+    battleThemeSource = new Audio('./assets/audio/battleTheme.mp3');
+        battleThemeSource.volume = 0.1;
+
+
+/* Picks a random number between two different numbers, Max / Min */
+function randomNum(max, min){
+    return Math.floor(Math.random() * (max - min) + min); 
+}
+
+
+/* picks a random number : 0 - 3*/
+function randomAttack() {
+    return Math.floor(Math.random() * 4);
+}
+
+
+/* Makes a class so you can quickly build pokemons */
+class Pokemon {
     constructor(pokemonName, frontImage, backImage, hp, moves) {
         this.name = pokemonName;
         this.frontImage = frontImage;
         this.backImage = backImage;
         this.hp = hp;
-        this.moves = moves
+        this.moves = moves;
     }
+    /* Where I learned it from : https://www.w3schools.com/js/js_classes.asp */
 }
 
-var charmander = new pokemon  (
-    "charmander", "./assets/charmanderFront.png", "./assets/charmanderBack.gif", 100,
+
+/* Makes a new array for a pokemon called Corviknight */
+var corviknight = new Pokemon  (
+    "corviknight", "./assets/corviknightFront.gif", "./assets/corviknightBack.gif", 100,
         [
-            ["Scratch", randomNum(25, 15)],
-            ["Flamethrower", randomNum(50, 5)],
-            ["Ember", randomNum(30, 15)],
-            ["Tackle", randomNum(10, 40)]
+            /* Corviknights moves, Max and min number ( randomNum() function ) */
+            ["Brave bird", 60, 10],
+            ["Body Press", 40, 12],
+            ["Roost", 30, 5],
+            ["Tackle", 20, 15]
         ]
 );
-var gengar = new pokemon (
-    "Gengar", "./assets/charmanderFront.png", "./assets/charmanderBack.gif", 100,
+
+
+/* Makes a new array for a pokemon called Aeorodactyl */
+var darkrai = new Pokemon (
+    "darkrai", "./assets/darkraiFront.gif", "./assets/darkraiBack.gif", 100,
         [
-            ["Scratch", randomNum(100, 40)],
-            ["Flamethrower", randomNum(100, 40)],
-            ["Ember", randomNum(10, 40)],
-            ["Tackle", randomNum(100, 40)]
+            ["Shadow ball", 50, 10],
+            ["Dream barrage", 40, 5],
+            ["Potion", 50, 10],
+            ["Shadow meteor", 40, 22]
         ]
-);
-startGame.addEventListener('click', function(){
-    document.getElementById("intro").style.display = 'none';
-    document.getElementById("selectPokemon").style.display = 'flex';
-});
-
-var attackOne = document.querySelector("#attackOne");
-var attackTwo = document.querySelector("#attackTwo");
-var attackThree = document.querySelector("#attackThree");
-var attackFour = document.querySelector("#attackFour");
-
-/*
+); 
 
 
-
-Try doing the same thing you did with attacks -> storing the variable -> 3 event listeners 
-for 3 pokemon but one function.
-
-
-
-
-*/
-function randomNum(max, min){
-     return Math.floor(Math.random() * (max - min) + min); 
+/* The starting screen */
+function pokemonSelection() {
+    backgroundImage.style.backgroundImage = "url('" + backgroundImages[0] + "')";
+    introGame.style.display = 'none';
+    selectPokemon.style.display = 'flex';
 }
 
-charmanderB.addEventListener('click', function(){
+
+/* When clicked on button that has var corviknightButton attached to it */
+corviknightButton.addEventListener("click", function(){
+
+    battleThemeSource.play();
+
     /* Hiding and revealing html elements */
-    document.getElementById("selectPokemon").style.display = 'none';
-    document.getElementById("heroAttacks").style.display = 'flex';
-    document.getElementById("heroHpContainer").style.display = 'flex';
-    document.getElementById("heroInfo").style.display = 'flex';
+    selectPokemon.style.display = 'none';
+    document.getElementById("battle").style.display = 'flex';
+    document.getElementById("battleUi").style.display = 'flex';        
+    backgroundImage.style.backgroundImage = "url('" + backgroundImages[1] + "')";
 
-    /* Visuals for Pikachu */
-        heroN.innerHTML = charmander.name;
-        heroHp.style.width = charmander.hp + "%";
-        heroI.src = charmander.backImage;
-        /* Attacks for Pikachu */
-        attackOne.innerHTML = charmander.moves[0][0];
-        attackTwo.innerHTML = charmander.moves[1][0];
-        attackThree.innerHTML = charmander.moves[2][0];
-        attackFour.innerHTML = charmander.moves[3][0];
-
-    /* "Animation" fpr enemy Pokemen */
     setTimeout(function(){
-        document.getElementById("enemyInfo").style.display = 'flex';
-        enemyN.innerHTML = gengar.name;
-        enemyHp.style.width = gengar.hp + "%";
-        enemyI.src = gengar.frontImage;
-    }, 1000); 
+        heroInfo.style.display = 'flex';
+
+        /* Visuals for Corviknight */
+        document.querySelector("#heroName").innerText = corviknight.name;
+        heroHp.style.width = corviknight.hp + "%";
+        heroImg.src = corviknight.backImage;
+
+        /* Attacks for Corviknight */
+        document.querySelector("#attackOne").innerText = corviknight.moves[0][0];
+        document.querySelector("#attackTwo").innerText = corviknight.moves[1][0];
+        document.querySelector("#attackThree").innerText = corviknight.moves[2][0];
+        document.querySelector("#attackFour").innerText = corviknight.moves[3][0];
+        
+        /* Animation for enemy Pokemen, on a timer */
+        setTimeout(function(){
+            document.querySelector("#enemyName").innerText = darkrai.name;
+            enemyInfo.style.display = 'flex';
+            enemyHp.style.width = darkrai.hp + "%";
+            enemyImg.src = darkrai.frontImage;
+        }, 1200); 
+
+    }, 1200); 
+}); 
+
+
+/* When user clicks on any attack, attach attack to variable and run heroDamageStep function */
+heroAttacks.addEventListener('click', function(e) {
+
+    let attackNumber = e.target.value;
+    const damage = randomNum (corviknight.moves[attackNumber][1], corviknight.moves[attackNumber][2]);
+    let heroDamage = damage;
+    let moveName = corviknight.moves[attackNumber][0];
+
+    /* Runs function heroDamagstep */
+    heroDamageStep(heroDamage, moveName, attackNumber) 
+
 });
 
 
-
-function move1(heroDamage) {
-    var heroDamage = charmander.moves[0][1];
+/* When hero Pokemon initiates an attack */
+function heroDamageStep(heroDamage, moveName, attackNumber){
     
-    setTimeout(function(){
-        attackOne.innerText = 'Used';
-        attackOne.style.background = 'gray'
-    }, 500); 
+    heroAttacks.style.display = 'none';
 
-    var heroAttack = true;
+    /* If the attackNumber number equals 1 of 3 damaging moves */
+    if ( corviknight.moves[attackNumber] === corviknight.moves[0] || corviknight.moves[attackNumber] === corviknight.moves[1] || corviknight.moves[attackNumber] === corviknight.moves[3]){
+        
+        /* Total hp of enemy monster minus the damage of hero attack */
+        var updatedEnemyHp = darkrai.hp - heroDamage;
+        darkrai.hp = updatedEnemyHp;
+        enemyHp.style.width = darkrai.hp + '%';
 
-    if(heroAttack == true) {
-        heroDamageStep(heroDamage)  
-    }
-}
+        /* Animation */
+        heroImg.style.width = "283px";
+        heroImg.style.bottom = "30%";
+        heroImg.style.left = "23%";
 
-function move2(heroDamage) {
-    var heroDamage = charmander.moves[1][1];
-    setTimeout(function(){
-        attackTwo.innerText = 'Used';
-        attackTwo.style.background = 'gray'
-    }, 500); 
-
-    var heroAttack = true;
-
-    if(heroAttack == true) {
-        heroDamageStep(heroDamage)
-    }
+    }   
     
-}
+    /* If the attackNumber number equals the only healing move */
+    else if ( corviknight.moves[attackNumber] === corviknight.moves[2]) {
+        
+        /* Total hp of hero hp plus the healing amount */
+        var updatedEnemyHp = corviknight.hp + heroDamage;
+        corviknight.hp = updatedEnemyHp;
+        heroHp.style.width = corviknight.hp + '%';  
 
-function move3(heroDamage) {
-    var heroDamage = charmander.moves[2][1];
-    setTimeout(function(){
-        attackThree.innerText = 'Used';
-        attackThree.style.background = 'gray'
-    }, 500); 
+        /* Animation */
+        heroImg.style.bottom = "10%";
 
-    var heroAttack = true;
+        /* A message showing that you used this move, and the amount you healed for. On a timeout. */
+        setTimeout(function(){
+            battleText.innerHTML = corviknight.name + " heals for " + heroDamage + " Hp!"  ;
 
-    if(heroAttack == true) {
-        heroDamageStep(heroDamage)
+        }, 2000);
     }
+
+    battleText.style.display = "block";
+    battleText.innerHTML = corviknight.name + " uses " + moveName + "!";
    
-}
+   
+    if ( heroDamage > 50 ){
+    /* If hero deals more than 50 damage */
 
-function move4() {
-    var heroDamage = charmander.moves[3][1];
+        /* Timeout so user doesn't get overwhelmed */
+        setTimeout(function(){
+            battleText.innerHTML = "Critical hit!";
+        }, 1000);
+    } else if (heroDamage < 20){
+    /* If hero deals less than 20 damage */
+
+         /* Timeout so user doesn't get overwhelmed */
+         setTimeout(function(){
+            battleText.innerHTML = "It wasn't very effective";
+        }, 1000);
+    }
+
+    /* If enemy hp gets below 40 */
+    if ( darkrai.hp <= 40 ) {
+
+        /* Change background color of hp bar */
+        enemyHp.style.background = "#ff8d14";
+
+    } else if ( darkrai.hp <= 10 ) {
+      /* If enemy hp gets below 10 */ 
+
+        /* Change background color of hp bar */
+        enemyHp.style.background = "#730707";
+    }
+
+
+    /* Delay timeout, give user time to breath */
     setTimeout(function(){
-        attackFour.innerText = 'Used';
-        attackFour.style.background = 'gray'
-    }, 500); 
 
-    var heroAttack = true;
+        /* If hp of enemy pokemon reaches 0 */
+        if (darkrai.hp <= 0){
+            enemyHp.style.width = 0;
 
-    if(heroAttack == true) {
-        heroDamageStep(heroDamage)
-    }
-}
+            /* Important timeout so all the changes don't overlay at once */
+            setTimeout(function(){
 
-
-attackOne.addEventListener('click', move1);
-attackTwo.addEventListener('click', move2);
-attackThree.addEventListener('click', move3);
-attackFour.addEventListener('click', move4);
-
-
-/*  Current problem, hp does not go - visually -> gengar.hp updates, though it doesnt reflect in CSS  */
-
-function heroDamageStep(heroDamage){
-
-    var updatedEnemyHp = gengar.hp - heroDamage; 
-
-    gengar.hp = updatedEnemyHp;
-
-    enemyHp.style.width = gengar.hp + '%';
-    
-    if (gengar.hp <= 0){
-        alert("Congrats! You have won this battle.");
-    }
-    else {
+                /* animation so it's visible when hero wins */
+                battleText.innerText = corviknight.name + " Has won this battle!";
+                enemyInfo.style.opacity = "0";
+                enemyImg.style.opacity = "0"
+                enemyHp.style.opacity = "0";
         
-        alert("You did " + updatedEnemyHp + " damage");
-        enemyBattleStep()
-    }
+                /* Important timeout so all the changes don't overlay at once */
+                setTimeout(function(){
 
-    enemyHp.style.width = updatedEnemyHp + '%';
+                    /* Game ends */
+                    alert("Congrats! Thank you for playing");
+                    /* Force restart */
+                    location.reload()
+            
+                }, 3000);
+            }, 2000);
+        } else { 
+
+            /* reverts animation */
+            heroImg.style.width = "326px";
+            heroImg.style.bottom = "14%";
+            heroImg.style.left = "6%";
+            
+            /* Run enemyBattleStep function */
+            enemyBattleStep()
+            
+        }
+    }, 1500);
 }
 
+
+/* Enemy pokemon intiates an attack */ 
 function enemyBattleStep(){
-    function getRandomInt() {
-        return Math.floor(Math.random() * 4);
-    }
+
+    let enemyAttackNumber = randomAttack(4);
+    let moveName = darkrai.moves[enemyAttackNumber][0];
+
+    /* creates a random number from 0 - 3 */
+   
   
-    document.getElementById("heroAttacks").style.display = 'none';
-    var attackId = getRandomInt(4);
-    var moveName = gengar.moves[attackId][0];
-    alert("Gengar uses " + moveName)
+    battleText.style.display = "none";
 
     setTimeout(function(){
-        if (attackId == 0) {
-            var updatedHeroHp = charmander.hp - gengar.moves[0][1];
-            heroHp.style.width = updatedHeroHp + '%';
-            alert(updatedHeroHp);
-        }
-        else if (attackId == 1) {
-            var updatedHeroHp = charmander.hp  - gengar.moves[1][1];
-            heroHp.style.width = updatedHeroHp + '%';
-            alert(updatedHeroHp);
-        }
-        else if (attackId == 2) {
-            var updatedHeroHp = charmander.hp - charmander.moves[2][1];
-            heroHp.style.width = updatedHeroHp + '%';
-            alert(updatedHeroHp);
-        }
-        else if (attackId == 3) {
-            var updatedHeroHp = charmander.hp - charmander.moves[3][1];
-            heroHp.style.width = updatedHeroHp + '%';
-            alert(updatedHeroHp);
-        }
-    }, 500); 
-     
-    if (heroHp <= 0){
-        alert("Congrats! You have won this battle.")
-    }
-    else {
-        document.getElementById("heroAttacks").style.display = 'flex';
         
-    }
+        battleText.style.display = "block";
+            
+        battleText.innerText = darkrai.name + " uses " + moveName + "!";
+        
+        /* if attack equals 0 - 3  */
+        if (enemyAttackNumber == 0 || enemyAttackNumber == 1 || enemyAttackNumber == 3) {
+            
+            var enemyDamage = randomNum(darkrai.moves[enemyAttackNumber][1], darkrai.hp, darkrai.moves[enemyAttackNumber][1]);
+            /* Hero hp minus Max / Min of attack */
+            var updatedHeroHp = corviknight.hp - enemyDamage;
+
+            corviknight.hp = updatedHeroHp;
+            heroHp.style.width = updatedHeroHp + "%";
+
+            enemyImg.style.width = "225px";
+            enemyImg.style.top = "18%";
+            enemyImg.style.right = "23%";
+
+        } else if (enemyAttackNumber == 2) {
+
+            var enemyDamage = randomNum(darkrai.moves[2][1], darkrai.hp, darkrai.moves[2][1]);
+            var updatedEnemyHp = darkrai.hp + enemyDamage;
+
+            darkrai.hp = updatedEnemyHp;
+            enemyHp.style.width = updatedEnemyHp + "%";
+
+           
+            enemyImg.style.top = "2%";      
+
+            setTimeout(function(){
+                battleText.innerHTML = darkrai.name + " heals for " + enemyDamage + " Hp!"  ;
+            }, 1000);
+        } 
+
+        if ( enemyDamage > 50 ){
+            /* If enemy does over 50 damage */ 
+                setTimeout(function(){
+                    battleText.innerHTML = "Critical hit!";
+
+                }, 1000);
+        } else if ( enemyDamage < 20 ){
+            /* If enemy does lower than 20 damage */ 
+
+            setTimeout(function(){
+                battleText.innerHTML = "It wasn't very effective..";
+
+            }, 1000);
+        }   
+
+
+        if ( corviknight.hp <= 40 ) {
+            /* If hero hp gets below 40 */ 
+
+            /* Change background color of hp bar */
+            heroHp.style.background = "#ff8d14";
+    
+        } else if ( corviknight.hp <= 10 ) {
+          /* If hero hp gets below 10 */ 
+    
+            /* Change background color of hp bar */
+            heroHp.style.background = "#730707";
+        }
+    
+       
+        /* If heromonster gets below 0, end game */
+        if  (corviknight.hp <= 0){
+            /* Animation for when hero dies*/
+                
+            heroHp.style.width = "0";
+
+            /* Important timeout so all the changes don't overlay at once */
+            setTimeout(function(){
+
+                /* animation so it's visible when enemy wins */
+                battleText.innerText = darkrai.name + " Has won this battle!";
+                heroInfo.style.opacity = "0";
+                heroImg.style.opacity = "0"
+                heroHp.style.opacity = "0";
+                enemyImg.style.top = "6%";
+                enemyImg.style.right = "6%";
+            
+                /* Important timeout so all the changes don't overlay at once */
+                setTimeout(function(){
+    
+                    /* Game ends */
+                    alert("Good try! Care for another go?");
+                    /* Force restart */
+                    location.reload()
+                
+                }, 3000);
+            }, 2000);
+        }
+        else {
+            setTimeout(function(){
+                /* Styling animation of enemy pokemon */
+                enemyImg.style.top = "6%";
+                enemyImg.style.right = "6%";
+                enemyImg.style.width = "178px";
+                battleText.style.display = "none";
+
+                /* Displays the overview of Hero attacks */
+                heroAttacks.style.display = 'flex';   
+            }, 1500); 
+        }
+    }, 3000);
+     
 }
+
+
+startGame.addEventListener('click', pokemonSelection);
+
+
+/* Sources 
+
+Audio
+    battleTheme.mp3 : https://www.youtube.com/watch?v=rXefFHRgyE0&t=80s 
+
+logo : https://en.wikipedia.org/wiki/Pok%C3%A9mon
+
+Pokemon gifs
+    corviknightFront.gif : https://pokemon.neoseeker.com/wiki/Corviknight
+    corviknightBack.gif : https://www.smogon.com/forums/threads/custom-3d-animated-renders.3526922/page-11
+
+    darkraiFront.gif : https://www.pokencyclopedia.info/en/index.php?id=sprites/491
+    darkraiBack.gif : https://www.pokencyclopedia.info/en/index.php?id=sprites/491
+
+Background Images
+    background.jpg : https://www.reddit.com/r/PokemonSwordAndShield/comments/de6qvy/lets_appreciate_the_beauty_that_is_the_galar/
+    background-1.jpg : https://hypebeast.com/2018/7/pokemon-lets-go-pikachu-eevee-nostalgia
+    background-2.png :https://aminoapps.com/c/pokeartanddrawing/page/blog/shiyaini/a7d0_X5C0uKY0m05nxlgjaka3j41ozMxq6
+
+*/ 
